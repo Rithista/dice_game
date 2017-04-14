c899da2377e1e45e27c226b4c3398c58a9697284
Harie <crazy.harry.jones@gmail.com>
2017-04-14 18:30:19 +0100

    Structure fixed and sorted

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ C# based 3 of a kind dice game
 
 Written in c# visual studio
 Download link is here for the 2017 edition: https://www.visualstudio.com/downloads/
+
+
+the design for this game is OOD (Object orientated design)
--- dice_game.cs
+++ dice_game.cs
@@ -6,184 +6,239 @@ using System.Windows.Input;
 namespace dice_game.cs
 {
 
-    public class Game
+  public class Game
+  {
+    public static int playerTotal;
+
+    private static List<Player> _players = new List<Player>();
+
+    public static void PrintStart()
     {
-            public static int playerTotal;
-            public static void PrintStart()
-            {
-                Console.Clear();
-                Console.WriteLine("==========================================================================================");
-                Console.WriteLine("\tWelcome to Harry's Dice Game");
-                Console.WriteLine("\tFirst to 50 points will win the game!");
-                Console.WriteLine("==========================================================================================");
+      Console.Clear();
+      Console.WriteLine("==========================================================================================");
+      Console.WriteLine("\tWelcome to Harry's Dice Game");
+      Console.WriteLine("\tFirst to 50 points will win the game!");
+      Console.WriteLine("==========================================================================================");
 
-            }
+    }
 
-        public static void Main()
+    public static void Main()
+    {
+
+      PrintStart();
+      Console.Write("Enter P to play against Players, or C to play against the Computer:");
+
+      //goto point to loop back over for validation 
+      playerInput0:
+
+      //User input
+      string playerInput = Console.ReadLine();
+      bool Opponent = true;
+
+      if (playerInput.Contains("p") || playerInput.Contains("P"))
+      {
+        Opponent = true;
+        Console.WriteLine("\nYou have selected to play against other Players!");
+      }
+      else if (playerInput.Contains("c") || playerInput.Contains("C"))
+      {
+        Opponent = false;
+        Console.WriteLine("\nYou have selected to play against the Computer!");
+      }
+      else
+      {
+        PrintStart();
+        Console.Write("Please enter a valid letter: ");
+        goto playerInput0;
+      }
+
+      //Player creation
+      playerTotal = 0;
+
+      if (Opponent == true)
+      {
+        Console.Write("\nPlease enter the amount of players you wish to play against: ");
+
+        playerTotal0:
+
+        try
         {
-
+          playerTotal = Convert.ToInt32(Console.ReadLine());
+          while (playerTotal < 2)
+          {
             PrintStart();
-            Console.Write("Enter P to play against Players, or C to play against the Computer:");
-
-        //goto point to loop back over for validation 
-        playerInput0:
-
-            //User input
-            string playerInput = Console.ReadLine();
-            bool Opponent = true;
-
-            if (playerInput.Contains("p") || playerInput.Contains("P"))
-            {
-                Opponent = true;
-                Console.WriteLine("\nYou have selected to play against other Players!");
-            }
-            else if (playerInput.Contains("c") || playerInput.Contains("C"))
-            {
-                Opponent = false;
-                Console.WriteLine("\nYou have selected to play against the Computer!");
-            }
-            else
-            {
-                PrintStart();
-                Console.Write("Please enter a valid letter: ");
-                goto playerInput0;
-            }
-
-            //Player creation
-            playerTotal = 0;
-            if (Opponent == true)
-            {
-                Console.Write("\nPlease enter the amount of players you wish to play against: ");
-
-            playerTotal0:
-
-                try
-                {
-                    playerTotal = Convert.ToInt32(Console.ReadLine());
-                    while (playerTotal < 2)
-                    {
-                        PrintStart();
-                        Console.Write("Please enter a value greater than 1: ");
-                        playerTotal = Convert.ToInt32(Console.ReadLine());
-                    }
-                }
-                catch
-                {
-                    PrintStart();
-                    Console.Write("Please enter an Integer: ");
-                    goto playerTotal0;
-                }
-
-                PrintStart();
-                Console.WriteLine("You are playing against {0} opponents, good luck!", playerTotal);
-            }
-
-            //####Game START#####
-
-            while (1 <= 50)//Turn.topScore()
-            {
-
-                Turn.takeTurn();
-                int[] results;
-                Roll r = new Roll();
-                results = r.roll5();
-
-
-
-                Console.WriteLine("Turn number is {0}", Turn.getTurnNumber());
-                string turnHistory = String.Join("|\n|", Turn.turnTotal);
-                turnHistory = string.Format("|{0}|", turnHistory);
-                Console.WriteLine(turnHistory);
-                //Console.WriteLine(Player.currentPlayer());
-                //Console.WriteLine("GOT TO HERE");
-                string rollOutcome = string.Join(", ", results);
-                Console.WriteLine("#################ROLLING################");
-                rollOutcome = string.Format("\t    | {0} |", rollOutcome);
-                Console.WriteLine("{0}", rollOutcome);
-                Console.WriteLine("#################ROLLING################");
-                Console.ReadLine();
-
-            }
-           // Console.WriteLine("Player {0] is the winner and has reached 50 points!", topPlayerNumber());
+            Console.Write("Please enter a value greater than 1: ");
+            playerTotal = Convert.ToInt32(Console.ReadLine());
+          }
         }
-    }
+        catch
+        {
+          PrintStart();
+          Console.Write("Please enter an Integer: ");
+          goto playerTotal0;
+        }
 
-    public class Player
+        PrintStart();
+        Console.WriteLine("You are playing against {0} opponents, good luck!", playerTotal);
+      }
+      for (int i = 1; i <= playerTotal; i++)
+      {
+        _players.Add(new Player(i));
+      }
+
+      //####Game START#####
+      bool isGameWon = false;
+
+      while (!isGameWon)//Turn.topScore()
+      {
+        for (int i = 0; i < playerTotal; i++)
+        {
+          Player p = _players.ElementAt(i);
+          p.takeTurn();
+          p.printScore();
+          isGameWon = p.isWinner();
+          if (isGameWon)
+          {
+            break;
+          }
+        }
+
+
+
+
+
+
+
+
+
+        /*Turn.takeTurn();
+        int[] results;
+        Roll r = new Roll();
+        results = r.roll5();
+
+
+
+        Console.WriteLine("Turn number is {0}", Turn.getTurnNumber());
+        string turnHistory = String.Join("|\n|", Turn.turnTotal);
+        turnHistory = string.Format("|{0}|", turnHistory);
+        Console.WriteLine(turnHistory);
+
+        //Console.WriteLine(Player.currentPlayer());
+        //Console.WriteLine("GOT TO HERE");
+
+        string rollOutcome = string.Join(", ", results);
+        Console.WriteLine("#################ROLLING################");
+        rollOutcome = string.Format("\t    | {0} |", rollOutcome);
+        Console.WriteLine("{0}", rollOutcome);
+        Console.WriteLine("#################ROLLING################");
+        Console.ReadLine();
+        */
+      }
+      // Console.WriteLine("Player {0] is the winner and has reached 50 points!", topPlayerNumber());
+    }
+  }
+
+  public class Player
+  {
+    private int _score = 0;
+    private int _playerNumber;
+
+
+    public void printScore()
     {
-        private static int totalPlayers = Game.playerTotal;
-        public static int currentPlayerTally = 1;
-
-        public static int currentPlayer()
-        {
-            for(int i = 1; i <= totalPlayers; i++)
-            {
-                currentPlayerTally = i;
-
-                if (i == totalPlayers)
-                {
-                    i = 1;
-                }
-            }
-            return currentPlayerTally;
-        }
+      Console.WriteLine("Player {0} score is {1}", _playerNumber, _score);
     }
-
-    public class Die
+    public Player(int number)
     {
-        private Random rnd;
-
-        public Die()
-        {
-            rnd = new Random();
-        }
-        public int roll()
-        {
-            return rnd.Next(1, 7);
-        }
+      _playerNumber = number;
     }
-
-    public class Turn
+    public int getScore()
     {
-        private static int i = 0;
-        public static List<int> turnTotal = new List<int>();
-        
-
-        public static int takeTurn()
-        {
-            return 1;
-        }
-
-        public static int getTurnNumber()
-        {
-            i += 1;
-            turnTotal.Add(i);
-            return i;
-
-        }
-       // public static int topScore()
-       // {
-       //
-       // }
+      return _score;
     }
 
-    public class Roll
+    public void takeTurn()
     {
-        private Die dice;
-        public int[] results;
-
-        public int[] roll5()
-        {
-            for (int i = 0; i <= 4; i++)
-            {
-                results[i] = dice.roll();
-            }
-            return results;
-        }
-
-        public Roll()
-        {
-            dice = new Die();
-            results = new int[5];
-        }
+      Console.WriteLine("Player {0}: taking turn", _playerNumber);
+      _score += 10;
+      Turn t = new Turn();
+      t.takeTurn();
     }
+    public bool isWinner()
+    {
+      if (_score >= 50)
+      {
+        Console.WriteLine("Player {0} is the winner", _playerNumber);
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+  }
+
+
+
+  public class Turn
+  {
+    private int _score = 0;
+
+    public int takeTurn()
+    {
+      Roll r = new Roll();
+      r.doRoll(5);
+      r.printRoll();
+      return _score;
+    }
+  }
+
+  public class Roll
+  {
+    private Die dice;
+    private List<int> _results = new List<int>();
+
+    public Roll()
+    {
+      dice = new Die();
+    }
+
+    public void printRoll()
+    {
+      string rollOutcome = string.Join(", ", _results);
+      rollOutcome = string.Format("\t    | {0} |", rollOutcome);
+      Console.WriteLine(rollOutcome);
+    }
+
+    public List<int> doRoll(int number)
+    {
+
+      _results.Clear();
+
+      for (int i = 0; i <= number; i++)
+      {
+        _results.Add(dice.roll());
+      }
+      return _results;
+    }
+  }
+
+  public class Die
+  {
+    private static Random rnd = new Random();
+    private int _value;
+    public Die()
+    {
+
+    }
+    public int roll()
+    {
+      _value = rnd.Next(1, 7);
+      return _value;
+    }
+    public void printDie()
+    {
+      Console.WriteLine("Dice {0}", _value);
+    }
+  }
 }
