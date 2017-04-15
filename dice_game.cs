79d5e0a175f0e0fcef4998794aba77ef5a570953
Harie <crazy.harry.jones@gmail.com>
2017-04-16 00:03:10 +0100

    changed classes but still not working

--- dice_game.cs
+++ dice_game.cs
@@ -9,6 +9,7 @@ namespace dice_game.cs
   public class Game
   {
     public static int playerTotal;
+    public static bool Opponent = true;
 
     private static List<Player> _players = new List<Player>();
 
@@ -33,7 +34,7 @@ namespace dice_game.cs
 
       //User input
       string playerInput = Console.ReadLine();
-      bool Opponent = true;
+
 
       if (playerInput.Contains("p") || playerInput.Contains("P"))
       {
@@ -78,8 +79,12 @@ namespace dice_game.cs
           goto playerTotal0;
         }
 
+
         PrintStart();
         Console.WriteLine("You are playing against {0} opponents, good luck!", playerTotal);
+      }else if (Opponent == false)
+      {
+        playerTotal = 2;
       }
       for (int i = 1; i <= playerTotal; i++)
       {
@@ -103,11 +108,18 @@ namespace dice_game.cs
           {
             break;
           }
-         // Console.WriteLine("Now its player {0}'s turn!", _players[i + 1]);
-          Console.WriteLine("Press enter to take turn");
-          Console.ReadLine();
+          // Console.WriteLine("Now its player {0}'s turn!", _players[i + 1]);
+          if(Opponent == true)
+          {
+            Console.WriteLine("Press enter to take turn");
+            Console.ReadLine();
+          }
         }
       }
+    }
+    public static int whoAmI()
+    {
+      return Convert.ToInt32(Opponent);
     }
   }
 
@@ -115,6 +127,7 @@ namespace dice_game.cs
   {
     private int _score = 0;
     private int _playerNumber;
+    private static int currentTurn;
 
 
     public void printScore()
@@ -124,6 +137,16 @@ namespace dice_game.cs
     public Player(int number)
     {
       _playerNumber = number;
+      int isComputer = 0;
+      if(_playerNumber == 2)
+      {
+        isComputer = 1;
+      }
+      if((isComputer == 1) && (Game.whoAmI() == 0))
+      {
+        _playerNumber = Convert.ToInt32("Computer");
+      }
+
     }
     public int getScore()
     {
@@ -132,11 +155,29 @@ namespace dice_game.cs
 
     public void takeTurn()
     {
-      Console.WriteLine("Player {0}: taking turn", _playerNumber);
-
       Turn t = new Turn();
+
+      if (t.findTurnNumber(_playerNumber) == 1)
+      {
+        currentTurn++;
+      }
+
+      Console.WriteLine("Player {0}: taking turn {1}", _playerNumber, currentTurn);
+
       _score += t.takeTurn();
     }
+    public int getPlayer()
+    {
+      return _playerNumber;
+    }
+
+
+
+    public int getTurn()
+    {
+      return currentTurn;
+    }
+
     public bool isWinner()
     {
       if (_score >= 50)
@@ -158,6 +199,24 @@ namespace dice_game.cs
     private int _score = 0;
     private static int diceFace;
     private Dictionary<int, int> _kind = new Dictionary<int, int>();
+    private string _historyRoll;
+    private string _history;
+
+
+
+
+    public int findTurnNumber(int number)
+    {
+      if (number == 1)
+      {
+        return 1;
+      }
+      else
+      {
+        return 0;
+      }
+
+    }
 
     public int takeTurn()
     {
@@ -165,9 +224,16 @@ namespace dice_game.cs
       Roll r = new Roll();
       results = r.doRoll(4);
       r.printRoll();
+      _historyRoll = string.Join(", ", results);
+      _history = string.Format("{0}\nPlayer {1}, Rolled {2} on turn {3}.", _history, Player.getPlayer(), _historyRoll, Player.getTurn());
       this.findKind(results);
+
+
+
+
+
       int hc = highestCount();
-      if(hc == 2)
+      if (hc == 2)
       {
         Console.WriteLine("\nYou found 2 {0}'s, press enter to re-roll 3 dice...", diceFace);
         Console.ReadLine();
@@ -177,29 +243,30 @@ namespace dice_game.cs
         hc = highestCount();
       }
 
-      if(hc == 3)
+      if (hc == 3)
       {
-        
+
         _score = 3;
         this._kind.Clear();
         Console.WriteLine("\nYou found 3 {0}'s and scored {1} points!", diceFace, _score);
       }
-      if(hc == 4)
+      if (hc == 4)
       {
-        
+
         _score = 6;
         this._kind.Clear();
 
         Console.WriteLine("\nYou found 4 {0}'s and scored {1} points!", diceFace, _score);
       }
-      if(hc == 5)
+      if (hc == 5)
       {
         _score = 12;
         this._kind.Clear();
 
         Console.WriteLine("\nYou found 5 {0}'s and scored {1} points!", diceFace, _score);
       }
-      if(hc == 1) {
+      if (hc == 1)
+      {
 
 
         this._kind.Clear();
@@ -207,12 +274,20 @@ namespace dice_game.cs
       }
       return _score;
     }
+    //##HISTORY BUILDER##
+    public void historyBuilder()
+    {
+      string history = string.Join(", ", _historyRoll);
+      Console.WriteLine(history);
+
+
+    }
 
     private void findKind(List<int> r)
     {
 
 
-      for(int i = 0; i < r.Count(); i++)
+      for (int i = 0; i < r.Count(); i++)
       {
         int v = r.ElementAt(i);
         if (_kind.ContainsKey(v))
@@ -232,7 +307,7 @@ namespace dice_game.cs
       int h = 0;
       foreach (KeyValuePair<int, int> entry in _kind)
       {
-        if(h < entry.Value)
+        if (h < entry.Value)
         {
           h = entry.Value;
         }
@@ -254,7 +329,7 @@ namespace dice_game.cs
     public void printRoll()
     {
       string rollOutcome = string.Join(", ", _results);
-      rollOutcome = string.Format("\t    | {0} |", rollOutcome);
+      rollOutcome = string.Format("!!!ROLLING| {0} |ROLLING!!!", rollOutcome);
       Console.WriteLine("\n{0}", rollOutcome);
     }
 
